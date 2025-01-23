cd16d76e00e6d21dc72f5e3dca362f117c47e750
Patrick Mukendi <mdipatrick5@gmail.com>
2025-01-23 16:33:50 +0200

    update

---
+++ readme.js
@@ -0,0 +1,65 @@
+module.exports = `### Hi there 👋
+
+I create application on the web and share my journey on [Twitter](https://mlv.sh/twitter) and [Youtube](https://mlv.sh/youtube) !
+
+And... I create online courses in french to help developers to master modern web technologies.
+
+### Contact me
+
+* [Twitter](https://mlv.sh/twitter)
+* [Youtube](https://mlv.sh/youtube)
+* [Mail](mailto:contact@melvynx.com)
+* [Linkedin](https://mlv.sh/linkedin)
+* [Thread](https://mlv.sh/thread)
+
+### My projects
+
+* [Melvynx](https://mlv.sh)
+
+My personal website, where you can find my projects, my blog and my resume.
+
+* [Codelynx](https://mlv.sh/codelynx)
+
+My blog and my newsletter website.
+
+* [Codeline](https://mlv.sh/codeline)
+
+My online courses plateforme. I created BeginReact, NextReact and BeginJavaScript.
+
+* [Chat2Code](https://mlv.sh/chat2code)
+
+A ChatGPT like application that create and generate component directly in the browser. Stop wasting time copy pasting code from ChatGPT to your editor, Chat2Code bring editor in the chat.
+
+* [QuizUp](https://mlv.sh/quizup)
+
+Create a quiz with multiple choice and open questions with input. The next generation of quiz.
+
+* [BulkCorrector](https://mlv.sh/bulkcorrector)
+
+Correct the grammar of an entire book in 5 minutes.
+
+* [LinkedLeads](https://mlv.sh/linkedleads)
+
+Accept all your invitations on LinkedIn.
+
+* [PowerPost](https://mlv.sh/powerpost)
+
+Start reading posts FASTER.
+
+* [PowerNote](https://mlv.sh/powernote)
+
+Create a daily note without any friction.
+
+### Visitor count
+
+<img src="https://profile-counter.glitch.me/Melvynx/count.svg" />
+
+Added the \`Mon 3 Jan 2022\`
+
+Last update on <#today_date>
+
+<#day_before_new_years> days before new years
+
+<#gabot_signing>
+`;
+ 
